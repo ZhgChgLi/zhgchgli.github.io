ac3f62cd3d169831fd5f38c626ca595fa516fc38
zhgchgli <zhgchgli@gmail.com>
2022-06-14 23:18:51 +0800

    update og image tools

--- tools/zhgchgli_optimize_footer.rb
+++ tools/zhgchgli_optimize_footer.rb
@@ -1,21 +1,34 @@
-
-
 class Main
     def run()
         files = Dir['_posts/*.md']
         files.each do |file|
-            puts "#{file} Optimze Done!"
+            
             lines = File.readlines(file)
-            File.open(file, 'w+') do |markdownFile|
-                lines.each do |line|
-                    if line.include? "延伸閱讀" or line.include? "本文同步發表於" or line.include? "Like Z Realm" or line.include? "有任何問題及指教歡迎與我聯絡。" or line.include? "View original post on Medium"
-                        id = file.split("-").last.split(".").first
-                        markdownFile.puts "\r\n\r\n===\r\n\r\n 本文首次發表於 Medium ➡️ [**前往查看**](https://medium.com/p/#{id})\r\n"
-                        break
-                    end
-                    markdownFile.puts line
+            newLines = []
+
+            isFoundFirstIMG = false
+            lines.each do |line|
+                if line.include? "延伸閱讀" or line.include? "本文同步發表於" or line.include? "Like Z Realm" or line.include? "有任何問題及指教歡迎與我聯絡。" or line.include? "View original post on Medium"
+                    id = file.split("-").last.split(".").first
+                    newLines.append("\r\n\r\n===\r\n\r\n 本文首次發表於 Medium ➡️ [**前往查看**](https://medium.com/p/#{id})\r\n")
+                    break
                 end
+
+                matchImage = line[/(\!\[){1}.*(\]\(){1}([^\"]*)(\".*\")*\){1}/,3]
+                if isFoundFirstIMG == false and !matchImage.nil?
+                    matchImage = matchImage.strip
+                    newLines.insert(3, "image:\r\n")
+                    newLines.insert(4, "  path: #{matchImage}\r\n")
+
+                    isFoundFirstIMG = true
+                end
+
+                newLines.append(line)
             end
+
+            File.open(file, 'w') { |f| f.write(newLines.join) }
+
+            puts "#{file} Optimze Done!"
         end
 
         puts "Optimze Markdown Footer Success!"
