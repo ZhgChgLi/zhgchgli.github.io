7fff40085edabe8cad713e8ff81147a54b7a6439
zhgchgli <zhgchgli@gmail.com>
2023-03-12 13:35:22 +0800

    update optimize tool

--- tools/zhgchgli_optimize_footer.rb
+++ tools/zhgchgli_optimize_footer.rb
@@ -7,7 +7,7 @@ class Main
             newLines = []
 
             lines.each do |line|
-                if line.include? "延伸閱讀" or line.include? "本文同步發表於" or line.include? "Like Z Realm" or line.include? "有任何問題及指教歡迎與我聯絡。" or line.include? "_Converted [Medium Post]("
+                if line.include? "延伸閱讀" or line.include? "本文同步發表於" or line.include? "Like Z Realm" or line.include? "有任何問題及指教歡迎與我聯絡。" or line.include? "converted from Medium by [ZMediumToMarkdown]"
                     id = file.split("-").last.split(".").first
                     newLines.append("\r\n\r\n===\r\n\r\n 本文首次發表於 Medium ➡️ [**前往查看**](https://medium.com/p/#{id}){:target=\"_blank\"}\r\n")
                     break
